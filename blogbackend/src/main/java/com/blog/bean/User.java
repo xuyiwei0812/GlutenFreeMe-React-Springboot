45066fad618ea8c076c46656a1304e7417493d75
xuyiwei0812 <lunaxu2001@gmail.com>
2023-12-18 21:03:51 +0800

    backend add springboot frame

---
+++ blogbackend/src/main/java/com/blog/bean/User.java
@@ -0,0 +1,14 @@
+package
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    Integer userId;
+    String username;
+    String password;
+}
