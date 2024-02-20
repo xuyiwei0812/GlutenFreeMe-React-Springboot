bab13e96a51fd00ccd6892a55c3284ad88a86edf
xuyiwei0812 <lunaxu2001@gmail.com>
2024-02-21 00:58:18 +0800

    update frontpage

--- blogfrontend/src/FrontPage.js
+++ blogfrontend/src/FrontPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carousel from "./Carousel";
+import RecipesPage from "./Recipes";
 
 const FrontPage = () => {
 return (
@@ -7,6 +8,9 @@ return (
 <br/>
 <br/>
         <Carousel />
+        <br/>
+        <h2 style={{marginLeft: '600px'}}>View Our Gluten Free Recipes</h2>
+        <RecipesPage />
     </div>
 );
 };
